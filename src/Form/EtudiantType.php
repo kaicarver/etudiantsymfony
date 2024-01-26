5d604874c4c333d265f923a799ec489270b99c56
Robert Carver <155527754+robertcarv@users.noreply.github.com>
2024-01-26 14:27:31 +0100

    exo form avec styles et bootstrap et un bouton qui marche pas

--- src/Form/EtudiantType.php
+++ src/Form/EtudiantType.php
@@ -4,18 +4,43 @@ namespace App\Form;
 
 use App\Entity\Etudiant;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+//use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class EtudiantType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('email')
-            ->add('fichier')
+            ->add('nom', TextType::class, [
+                "attr"=>["class"=>"form-control","id"=>"nomet"], 
+                "label"=>"Nom de famille",
+                "label_attr"=>["class"=>"mt-2 text-white"],
+                ])
+            ->add('prenom', TextType::class, [
+                "attr"=>["class"=>"form-control","id"=>"nomet"], 
+                "label"=>"Prénom",
+                "label_attr"=>["class"=>"mt-2 text-white"],
+                ])
+            ->add('email', EmailType::class, [
+                "attr"=>["class"=>"form-control","id"=>"nomet"], 
+                "label"=>"Email",
+                "label_attr"=>["class"=>"mt-2 text-white"],
+                ])
+            ->add('fichier', TextType::class, [
+                "attr"=>["class"=>"form-control","id"=>"nomet"], 
+                "label"=>"Fichier",
+                "label_attr"=>["class"=>"mt-2 text-white"],
+                ])
+            ->add('save', SubmitType::class, [
+                "attr"=>["class"=>"btn btn-primary mt-2 mb-3","id"=>"save"],
+                "label"=>"Ajouter",
+                ])
         ;
     }
 
